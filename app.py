4aa720f405276bbd6bff029be70b450dea86d8d4
ttgomes <83037084+ttgomes@users.noreply.github.com>
2021-08-04 21:54:33 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, redirect,request
+from flask_mail import Mail, Message
+app = Flask(__name__)
+
+mail_settings ={
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME":"thiagoblue00@gmail.com",
+    "MAIL_PASSWORD":"56705w00"
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
+
+class Contato: 
+    def __init__ (self,nome,email,assunto,mensagem):
+        self.nome = nome
+        self.email = email
+        self.assunto = assunto
+        self.mensagem = mensagem
+        
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/send", methods=["GET","POST"])
+def send():
+    if request.method == "POST":
+        formContato = Contato(
+        request.form["nome"],
+        request.form["email"],
+        request.form["assunto"],
+        request.form["mensagem"]
+        )
+
+        msg = Message(
+            sender=app.config.get("MAIL_USERNAME"),
+            recipients=["tgomescontrucao@gmail.com"],
+            body=f'''{formContato.nome} enviou a seguinte mensagem:
+            {formContato.mensagem}'''
+        )
+
+        mail.send(msg)
+    return render_template('send.html', formContato=formContato)
+
+if __name__ == '__main__':
+    app.run(debug=True)
